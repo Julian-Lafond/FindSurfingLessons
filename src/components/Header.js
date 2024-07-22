4fa3695227c8b62d62dc3f34546263a7215a9e43
Julian-Lafond <jmlafond@ucsc.edu>
2024-07-22 13:06:58 -0700

    first push with nav bar

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function(){
+    return(
+        <div className = "header">
+            <h1>Find Surfing Lessons</h1>
+            <div className = "header-tags">
+                <h3>Home</h3>
+                <h3>Apply To Coach</h3>
+                <h3>Find Surfing Lessons</h3>
+            </div>
+        </div>
+        
+    )
+}
