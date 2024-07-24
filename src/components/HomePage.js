3d17f594cdd5a7537db3948769067296454a7cbd
Julian-Lafond <jmlafond@ucsc.edu>
2024-07-23 17:13:36 -0700

    cleaned up with partitions

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,8 +4,10 @@ export default function(){
             <div className = "surf-lesson-image">
                 <img src = "images/surf-lesson.jpeg" alt = "Surfing Lesson"/>
             </div>
-            <h1>Find A Surfing Coach Near You!</h1>
-            <button>Find Your Surfing Coach</button>
+            <div className = "home-page-text">
+                <h1>Find A Surfing Coach Near You!</h1>
+                <button>Find Your Surfing Coach</button>
+            </div>
         </div>
     )
 }
