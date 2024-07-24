ceabe14b1dd0f7291f6f5c3842a324e494c96519
Julian-Lafond <jmlafond@ucsc.edu>
2024-07-24 16:15:20 -0700

    implemented apply to become a surf coach section

--- src/components/ApplyToCoach.js
+++ src/components/ApplyToCoach.js
@@ -1,7 +1,14 @@
 export default function(){
     return(
-        <div>
-            <p>Apply To Coach Page</p>
+        <div className = "apply-to-coach-container"> 
+            <div className = "apply-to-coach">
+                <h1>Apply To Become A Surf Coach</h1>
+                <h3>Post Your Own Free Coach Page</h3>
+                <h3>Find New Surfing Students</h3>
+                <h3>Teach Them at Your Own Convenience</h3>
+                <h3>Get Paid Directly From Students</h3>
+                <h3>Earn 100% of Lessons</h3>
+            </div>
         </div>
     )
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
 export default function(){
     return(
-        <div className = "home-page">
-            <div className = "surf-lesson-image">
-                <img src = "images/surf-lesson.jpeg" alt = "Surfing Lesson"/>
-            </div>
-            <div className = "home-page-text">
-                <h1>Find A Surfing Coach Near You!</h1>
-                <button>Find Your Surfing Coach</button>
+        <div className="home-page">
+            <div className="home-page-container">
+                <div className="surf-lesson-image">
+                    <img src="images/surf-lesson.jpeg" alt="Surfing Lesson"/>
+                </div>
+                <div className="home-page-text">
+                    <h1>Find A Surfing Coach Near You!</h1>
+                    <Link to = "/start-your-journey">
+                        <button>Find Your Surfing Coach</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
